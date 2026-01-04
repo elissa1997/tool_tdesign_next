dd83779606a415d6ad28b80520b6cfdca76fe43f
elissa1997 <524948583@qq.com>
2026-01-04 10:12:24 +0800

    fix: grid样式bug

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import router from '@/router/index.js'
 import 'tdesign-vue-next/es/style/index.css';
 import App from './App.vue'
+import tdRowFix from '@/directives/tdRowFix.js'
 
 import '@/assets/style/index.scss'
 
@@ -10,5 +11,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.directive('td-row-fix', tdRowFix)
 
 app.mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -28,10 +28,22 @@ const toolList = [
 <template>
 
   <div class="index">
-    <div class="card" v-for="item in toolList" :key="item.name">
-      <svgIcon class="icon" :name="item.icon"/>
-      <div class="name">{{ item.title }}</div>
-    </div>
+    <t-row v-td-row-fix :gutter="[20, 24]">
+      <t-col :xs="12" :sm="6" :md="3" :lg="3" :xl="3"  v-for="item in toolList" :key="item.name">
+
+        <div class="card">
+          <div class="header">
+            <svgIcon class="icon" :name="item.icon"/>
+            <div class="name">{{ item.title }}</div>
+          </div>
+
+          <div class="body">
+
+          </div>
+        </div>
+
+      </t-col>
+    </t-row>
 
 
   </div>
@@ -39,12 +51,40 @@ const toolList = [
 
 <style scoped lang="scss">
   .index {
+    @include respond-to('phone') {
+      padding: var(--td-pop-padding-xl);
+    }
+
+    @include respond-to('desktop') {
+      padding: var(--td-size-12) var(--td-size-16);
+    }
+
+    box-sizing: border-box;
     .card {
-      .icon {
-        width: 40px;
-        height: 40px;
-        color: #4fd6ff;
+      width: 100%;
+      padding: var(--td-pop-padding-xxl);
+      box-sizing: border-box;
+      border-radius: var(--td-radius-medium);
+      background-color: var(--td-bg-color-container);
+      border: 1px solid var(--td-brand-color-2);
+      .header {
+        display: flex;
+        align-items: center;
+        .icon {
+          width: 40px;
+          height: 40px;
+          color: var(--td-brand-color-8);
+          margin-right: var(--td-comp-paddingLR-l);
+        }
+
+        .name {
+          color: var(--td-text-color-primary);
+          font-size: var(--td-font-size-title-large);
+          font-weight: bold;
+        }
       }
+
+
     }
   }
 </style>
